666a9fb4bbe968b0f9de345963357aba0f30d42d
kingdaro <kingdaro@gmail.com>
2017-12-29 16:22:40 -0500

    rename NewTodoForm -> TodoActions

--- src/todos/components/App.tsx
+++ src/todos/components/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
 
-import { NewTodoForm } from './NewTodoForm'
+import { TodoActions } from './TodoActions'
 import { TodoList } from './TodoList'
 
 export function App() {
   return (
     <>
       <h1>awesome todo app</h1>
-      <NewTodoForm />
+      <TodoActions />
       <TodoList />
     </>
   )
--- src/todos/components/TodoActions.tsx
+++ src/todos/components/TodoActions.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { Dispatch } from '../../store'
 import { StoreConsumer } from '../../storeContext'
 
-export function NewTodoForm() {
+export function TodoActions() {
   type FormValues = { newTodoText: string }
 
   const handleSubmit = (dispatch: Dispatch) => (
