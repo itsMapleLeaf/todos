cef54b7358c4e5d1e185ddcdf77b2a6549da139d
kingdaro <kingdaro@gmail.com>
2017-12-29 18:26:20 -0500

    split out NewTodoForm

---
+++ src/todos/components/NewTodoForm.tsx
@@ -0,0 +1,28 @@
+import { Field, Form, Formik, FormikActions, FormikProps } from 'formik'
+import * as React from 'react'
+
+import { StoreConsumer } from '../../storeContext'
+
+type FormValues = { newTodoText: string }
+
+export function NewTodoForm() {
+  return (
+    <StoreConsumer>
+      {({ dispatch }) => {
+        const onSubmit = (values: FormValues, actions: FormikActions<FormValues>) => {
+          actions.setFieldValue('newTodoText', '')
+          dispatch({ type: 'ADD_TODO', text: values.newTodoText })
+        }
+
+        const render = (props: FormikProps<FormValues>) => (
+          <Form style={{ display: 'inline-block' }}>
+            <Field name="newTodoText" type="text" placeholder="What should be done?" />{' '}
+            <button type="submit">Add Todo</button>
+          </Form>
+        )
+
+        return <Formik initialValues={{ newTodoText: '' }} onSubmit={onSubmit} render={render} />
+      }}
+    </StoreConsumer>
+  )
+}
--- src/todos/components/TodoActions.tsx
+++ src/todos/components/TodoActions.tsx
@@ -1,37 +1,14 @@
-import { Field, Form, Formik, FormikActions, FormikProps } from 'formik'
 import * as React from 'react'
 
-import { Dispatch } from '../../store'
 import { StoreConsumer } from '../../storeContext'
+import { NewTodoForm } from './NewTodoForm'
 
 export function TodoActions() {
-  type FormValues = { newTodoText: string }
-
-  const handleSubmit = (dispatch: Dispatch) => (
-    values: FormValues,
-    actions: FormikActions<FormValues>,
-  ) => {
-    actions.setFieldValue('newTodoText', '')
-    dispatch({ type: 'ADD_TODO', text: values.newTodoText })
-  }
-
-  const renderForm = (props: FormikProps<FormValues>) => (
-    <Form style={{ display: 'inline-block' }}>
-      <Field name="newTodoText" type="text" placeholder="What should be done?" />{' '}
-      <button type="submit">Add Todo</button>
-    </Form>
-  )
-
   return (
     <StoreConsumer>
       {({ dispatch }) => (
         <>
-          <Formik
-            initialValues={{ newTodoText: '' }}
-            onSubmit={handleSubmit(dispatch)}
-            render={renderForm}
-          />
-
+          <NewTodoForm />
           <button onClick={() => dispatch({ type: 'CLEAR_FINISHED_TODOS' })}>
             Clear Finished Todos
           </button>
