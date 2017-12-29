80b5ad9c4011c76315427326e89271cbde960b25
kingdaro <kingdaro@gmail.com>
2017-12-29 15:55:39 -0500

    improve webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,45 @@
-module.exports = {
-  entry: './src/index',
-  output: {
-    path: __dirname + '/build',
-    filename: 'bundle.js',
-    publicPath: '/build/'
-  },
-  module: {
-    rules: [
-      { loader: 'ts-loader', test: /\.tsx?$/ }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+const webpack = require('webpack')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = env => {
+  const config = {
+    entry: {
+      app: './src/index',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: __dirname + '/build',
+    },
+    module: {
+      rules: [{ loader: 'ts-loader', test: /\.tsx?$/ }],
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx'],
+    },
+    plugins: [new webpack.NamedModulesPlugin()],
+    devtool: 'eval-source-map',
   }
+
+  if (env.production) {
+    config.plugins = [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': `"production"`,
+      }),
+
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'lib',
+        minChunks: ({ resource }) => resource && resource.includes('node_modules'),
+      }),
+
+      new webpack.optimize.ModuleConcatenationPlugin(),
+
+      new UglifyPlugin({
+        sourceMap: true,
+      }),
+    ]
+
+    config.devtool = 'source-map'
+  }
+
+  return config
 }
